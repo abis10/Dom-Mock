4110da70c25020d8d2da999504b5f2b726736f36
abis10 <70681025+abis10@users.noreply.github.com>
2020-10-16 18:06:44 -0700

    Initial commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+const x = document.querySelector("button")
+// x.addEventListener("click", function(){
+//     alert("Submitted")
+// })
+
+
+
+x.addEventListener("click", function(){
+   alert(document.getElementById("user").value)
+})
